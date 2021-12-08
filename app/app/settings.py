d21a595b49795a5bfc187f8f33774df5239d7575
sedin-travelperk <edin.sehovic@travelperk.com>
2021-12-08 10:12:27 +0100

    created user app

--- app/app/settings.py
+++ app/app/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'rest_framework',
+    'rest_framework.authtoken',
+    'core',
+    'user'
 ]
 
 MIDDLEWARE = [
@@ -77,11 +80,11 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'PASSWORD': os.environ.get('DB_PASS'),
         'HOST': os.environ.get('DB_HOST'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
-
+        'PASSWORD': os.environ.get('DB_PASS'),
+        'PORT': os.environ.get('DB_POST'),
     }
 }
 
