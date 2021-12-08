9b6ab7915512756ba6e9b7e47ae3b49a74e5f184
sedin-travelperk <edin.sehovic@travelperk.com>
2021-12-08 16:53:03 +0100

    create ingredients added

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -29,7 +29,11 @@ class TagViewSet(
         )
 
 
-class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class IngredientViewSet(
+    viewsets.GenericViewSet,
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin
+):
     """Manage ingredients in the databasae"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
@@ -39,3 +43,9 @@ class IngredientViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     def get_queryset(self):
         """Return objects for the current authenticated user"""
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """Create a new ingredient"""
+        serializer.save(
+            user=self.request.user
+        )
