88ab108dec4821f21534a86d3216c3c69fd40e33
sedin-travelperk <edin.sehovic@travelperk.com>
2021-12-08 10:41:29 +0100

    create user api added

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -5,7 +5,7 @@ from django.urls import reverse
 from rest_framework.test import APIClient
 from rest_framework import status
 
-CREATE_USER_API = reverse('user:create')
+CREATE_USER_URL = reverse('user:create')
 
 
 def create_user(**params):
@@ -20,14 +20,12 @@ class PublicUserApiTests(TestCase):
 
     def test_create_valid_user_success(self):
         """Test creating user with valid payload is successful"""
-
         payload = {
-            'email': 'test@test.com',
-            'password': 'test',
+            'email': 'test@londonappdev.com',
+            'password': 'testpass',
             'name': 'Test name'
         }
-
-        res = self.client.post(CREATE_USER_API, payload)
+        res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_201_CREATED)
         user = get_user_model().objects.get(**res.data)
@@ -35,31 +33,31 @@ class PublicUserApiTests(TestCase):
         self.assertNotIn('password', res.data)
 
     def test_user_exists(self):
-        """Test creating user that already exists fails"""
+        """Test creatinga  user that already exists fails"""
         payload = {
-            'email': 'test@test.com',
-            'password': 'test'
+            'email': 'test@londonappdev.com',
+            'password': 'testpass',
+            'name': 'Test',
         }
+        create_user(**payload)
 
-        res = self.client.post(CREATE_USER_API, payload)
+        res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_password_too_short(self):
         """Test that the password must be more than 5 characters"""
         payload = {
-            'email': 'test@test.com',
-            'password': 'ts'
+            'email': 'test@londonappdev.com',
+            'password': 'pw',
+            'name': 'Test',
         }
-
-        res = self.client.post(CREATE_USER_API, payload)
+        res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
-
         user_exists = get_user_model().objects.filter(
             email=payload['email']
         ).exists()
-
         self.assertFalse(user_exists)
 
 
